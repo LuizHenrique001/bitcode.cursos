b779d303fa5238157b080f31d0343c812a96b1c7
LuizHenrique001 <luizhenrique9x08@gmail.com>
2022-09-14 01:28:14 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ js/cursos.js
@@ -0,0 +1,33 @@
+const cursos = [
+    {   
+        img: 'images/img-curso-devweb.webp',
+        nome: 'Desenvolvimento Web',
+        info: `
+        HTML, CSS e JavaScript`,
+        link: `https://go.hotmart.com/S74778753M?dp=1`
+    }
+]
+
+
+iniciarBitCode = () => {
+    let containerCursos = document.querySelector('main')
+    cursos.map((val) =>{
+        containerCursos.innerHTML += `
+        <div class="container-cursos">
+            <img src="${val.img}" alt="Curso">
+            <p class="nome-curso">
+                ${val.nome}
+            </p>
+            <p class="info-curso">
+                ${val.info}
+            </p>
+            <p class="btn-comprar-curso">
+                <a href="${val.link}">Comprar</a>
+            </p>
+        </div>
+        `
+
+    })
+}
+
+iniciarBitCode()
