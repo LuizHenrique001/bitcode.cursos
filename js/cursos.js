5d917ab0c6537c319b48fc68b9a36d6a820a73bd
LuizHenrique <luizhenrique9x08@gmail.com>
2022-09-14 20:41:19 -0300

    Mais cursos

--- js/cursos.js
+++ js/cursos.js
@@ -3,7 +3,7 @@ const cursos = [
         img: 'images/img-curso-devweb.webp',
         nome: 'Desenvolvimento Web',
         info: `
-        HTML, CSS e JavaScript`,
+        Curso HTML, CSS e JavaScript.`,
         link: `https://go.hotmart.com/S74778753M?dp=1`
     },
 
@@ -11,8 +11,29 @@ const cursos = [
         img: 'images/img-curso-php.jpg',
         nome: 'Curso PHP',
         info: `
-        Do básico ao avançado em PHP`,
+        Do básico ao avançado em PHP.`,
         link: `https://go.hotmart.com/U74787224L`
+    },
+
+    {
+        img: 'images/img-bancodedados.png',
+        nome: 'Curso Banco de Dados',
+        info: `Aprenda Banco de Dados curso completo.`,
+        link: `https://go.hotmart.com/C74818226Y`
+    },
+
+    {
+        img: 'images/img-java.png',
+        nome: 'Curso JAVA',
+        info: `Curso completo de JAVA Vitalício.`,
+        link: `https://go.hotmart.com/M74819032Q?dp=1`
+    },
+
+    {
+        img: 'images/img-python.png',
+        nome: 'Curso de Python',
+        info: `Programe em Python do Básico ao Avançado.`,
+        link: `https://go.hotmart.com/U74819699C`
     }
 ]
 
