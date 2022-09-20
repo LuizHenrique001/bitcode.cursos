4985005cb7dc9a8faf5f2dedb84d091ac82ecfee
LuizHenrique <luizhenrique9x08@gmail.com>
2022-09-19 21:12:05 -0300

    A

--- js/query.js
+++ js/query.js
@@ -1,9 +1,15 @@
 let navbar = document.querySelector('div.menu-nav')
+let ul = document.querySelector('ul')
+let li = document.querySelector('li')
 clickMenu = () => {
     if(navbar.style.display == 'block') {
         navbar.style.display = 'none'
+        ul.style.display = 'none'
+        li.style.display = 'none'
     }else {
         navbar.style.display = 'block'
+        ul.style.display = 'block'
+        li.style.display = 'block'
     }
 }
 
